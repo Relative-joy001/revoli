62249471f4ff74c495cd8bf96ec63de7b43ddbed
RelativelyJoy001 <oguntim@gmail.com>
2024-08-16 21:19:28 -0400

    created a env file for ApI keys and a sample arrow function to test the index file.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+// const Star = () => React.createElement('div', null, 'This is a little star');
+
+// const App = () => {
+//   return Star();
+// } 
 function App() {
   return (
     <div className="App">
