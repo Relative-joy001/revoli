80e10c6fc5925aa04890f3b2a8642e6e37f96f60
RelativelyJoy001 <oguntim@gmail.com>
2024-08-16 21:40:09 -0400

    Global Style created and import into App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
-// const Star = () => React.createElement('div', null, 'This is a little star');
+// Styles
 
-// const App = () => {
-//   return Star();
-// } 
+import { GlobalStyle } from './GlobalStyle';
+
+
 function App() {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle />
     </div>
   );
 }
 
 export default App;
+
+
+
+// sample arrow function
+// const Star = () => React.createElement('div', null, 'This is a little star');
+
+// const App = () => {
+//   return Star();
+// } 
