bb6fc0146fd6d31b0f880e625b454fcb7a237a2e
RelativelyJoy001 <oguntim@gmail.com>
2024-08-17 17:51:45 -0400

    created a hero image folder and file to set the back drop image in the component header

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 // Components
+import HeroImage from "./HeroImage";
 
 //Hook
 import { useHomeFetch } from '../Hooks/useHomeFetch';
@@ -18,7 +19,22 @@ const Home = () => {
     
     console.log(state);
 
-    return <div>Home Page</div>
+    return (
+        <>
+            {state.results[0] ?
+                <HeroImage 
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                /> 
+            : null
+            }
+        
+        
+        </>
+
+
+    )
 };
 
 export default Home;
