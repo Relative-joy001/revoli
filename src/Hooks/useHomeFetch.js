b1f227434415505986689aa4cac99956350ce04a
RelativelyJoy001 <oguntim@gmail.com>
2024-09-03 00:42:44 -0400

    creaate  a control component, timer and input field which is the search bar

--- src/Hooks/useHomeFetch.js
+++ src/Hooks/useHomeFetch.js
@@ -11,9 +11,12 @@ const initialState = {
 };
 
 export const useHomeFetch = () => {
+    const [searchTerm, setSearchTerm] = useState("");
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+
+    console.log(searchTerm);
 
     const fetchMovies = async (page, searchTerm = '') => {
         try {
@@ -40,7 +43,7 @@ export const useHomeFetch = () => {
 
     }, []);
 
-    return { state, loading, error };
+    return { state, loading, error, setSearchTerm };
 
 
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
+import SearchBar from "./SearchBar";
 
 //Hook
 import { useHomeFetch } from '../Hooks/useHomeFetch';
@@ -18,7 +19,7 @@ import NoImage from '../images/no_image.jpg';
 
 
 const Home = () => {
-    const { state, loading, error } = useHomeFetch();
+    const { state, loading, error, setSearchTerm } = useHomeFetch();
     
     console.log(state);
 
@@ -31,6 +32,7 @@ const Home = () => {
                 text={state.results[0].overview}
               /> 
             ) : null}
+            <SearchBar setSearchTerm={setSearchTerm}/>
             <Grid header='Popular Movies'>
               {state.results.map(movie => (
                 <Thumb 
