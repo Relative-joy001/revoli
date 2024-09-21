8928e37a7ee4aab1c9db76aafe5c22c1166f98d7
RelativelyJoy001 <oguntim@gmail.com>
2024-09-20 22:14:05 -0400

    created router for the movie thumbnials and header making them clickable

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 import RMDBLogo from  '../../images/react-movie-logo.svg';
 
@@ -10,7 +11,10 @@ import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
 const Header = () => (
     <Wrapper>
         <Content>
+            <Link to="/">
+            
             <LogoImg src={RMDBLogo} alt='rmdb/revoli logo' />
+            </Link>
             <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
 
         </Content>
